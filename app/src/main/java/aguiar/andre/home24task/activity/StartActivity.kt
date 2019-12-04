661e98719987dc813dcd9c34e15ed7ee81ec3e2d
Andre Wescley <andrewescley@gmail.com>
2019-12-04 01:33:20 -0300

    ˜Initial˜

---
+++ app/src/main/java/aguiar/andre/home24task/activity/StartActivity.kt
@@ -0,0 +1,13 @@
+package aguiar.andre.home24task.activity
+
+import aguiar.andre.home24task.R
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
