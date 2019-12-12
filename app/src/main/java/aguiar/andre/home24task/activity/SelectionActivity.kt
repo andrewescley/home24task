64d958678ace29ee4c41c8c48b172a28af1a2040
Andre Wescley <andrewescley@gmail.com>
2019-12-11 22:44:37 -0300

    update gradle

--- app/src/main/java/aguiar/andre/home24task/activity/SelectionActivity.kt
+++ app/src/main/java/aguiar/andre/home24task/activity/SelectionActivity.kt
@@ -1,25 +1,26 @@
 package aguiar.andre.home24task.activity
 
+
 import aguiar.andre.home24task.ArticlesService
 import aguiar.andre.home24task.R
-import aguiar.andre.home24task.dataclass.Articles
-import aguiar.andre.home24task.dataclass.ArticlesSku
 import aguiar.andre.home24task.dataclass.HomeApiResponse
+
 import android.content.Context
-import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.ImageView
+
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_start.*
+import com.squareup.picasso.Picasso
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import com.squareup.picasso.Picasso
 
+
+@Suppress("UNREACHABLE_CODE")
 class SelectionActivity : AppCompatActivity() {
 
     private var articlesData: TextView? = null
@@ -43,8 +44,8 @@ class SelectionActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    internal fun getEmbedded() {
-        var listArticle = ArrayList<Articles>()
+    fun getEmbedded() {
+        //var listArticle: ArrayList<Articles> = ArrayList<Articles>()
         val retrofit = Retrofit.Builder()
             .baseUrl(BaseUrl)
             .addConverterFactory(GsonConverterFactory.create())
@@ -52,16 +53,20 @@ class SelectionActivity : AppCompatActivity() {
         val service = retrofit.create(ArticlesService::class.java)
         val call = service.getEmbedded(appDomain, locale, limit)
         call.enqueue(object : Callback<HomeApiResponse> {
-            override fun onResponse(call: Call<HomeApiResponse>, response: Response<HomeApiResponse>) {
+            override fun onResponse(
+                call: Call<HomeApiResponse>,
+                response: Response<HomeApiResponse>
+            ) {
+                //TODO: implement later
                 if (response.code() == 200) {
-                    val homeApiResponse = response.body()!!
+                    val homeApiResponse: HomeApiResponse = response.body()!!
 
-                    listArticle = homeApiResponse._embedded!!.articles
+                    val listArticle = homeApiResponse._embedded!!.articles
 
                     for (article in listArticle) {
                         for(media in article.media) {
 
-                            val imageViewCard = findViewById(R.id.imageArticle) as ImageView
+                            val imageViewCard = findViewById<ImageView>(R.id.imageArticle)
                             Picasso.with(context) .load(media.uri).into(imageViewCard)
                         }
                     }
@@ -70,7 +75,6 @@ class SelectionActivity : AppCompatActivity() {
                     articlesData!!.text = stringBuilder
                 }
             }
-
             override fun onFailure(call: Call<HomeApiResponse>, t: Throwable) {
 
             }
--- app/src/main/java/aguiar/andre/home24task/dataclass/DataClass.kt
+++ app/src/main/java/aguiar/andre/home24task/dataclass/DataClass.kt
@@ -25,28 +25,8 @@ class Articles {
     var media  = ArrayList<Media>()
 }
 
-class ArticlesSku {
-    @SerializedName("sku")
-    var sku: String? = null
-    @SerializedName("title")
-    var title: Title? = null
-    @SerializedName("images")
-    var image  = ArrayList<Image>()
-}
-
-class Image {
-    @SerializedName("url")
-    var url: String? = null
-}
-
 class Media {
     @SerializedName("uri")
     var uri: String? = null
 }
 
-class Title {
-    @SerializedName("cleanName")
-    var cleanName: String? = null
-    @SerializedName("variantName")
-    var variantName: String? = null
-}
